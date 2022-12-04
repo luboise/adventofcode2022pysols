78ed1de4a3dcda2978a78dc8bbb803460db6fe52
Lucas Rodiadis <lucas2@live.com.au>
2022-12-04 21:20:03 +1100

    The funny

--- Day 4/golf.py
+++ Day 4/golf.py
@@ -6,8 +6,8 @@ def clamp(low, high, contain_vals, *vals):
 		return len(vals) == 2 and low <= vals[0] <= vals[1] <= high
 	return any([low <= val <= high for val in vals])
 
-total = len([True for vals in vals_list if clamp(vals[0], vals[1], True, *vals[2:]) or clamp(vals[2], vals[3], True, *vals[:2])])
-total_overlapping = len([True for vals in vals_list if clamp(vals[0], vals[1], False, *vals[2:]) or clamp(vals[2], vals[3], False, *vals[:2])])
+total = sum([clamp(vals[0], vals[1], True, *vals[2:]) or clamp(vals[2], vals[3], True, *vals[:2]) for vals in vals_list])
+total_overlapping = sum([clamp(vals[0], vals[1], False, *vals[2:]) or clamp(vals[2], vals[3], False, *vals[:2]) for vals in vals_list])
 
 print(total, total_overlapping, sep = "\n")
 
