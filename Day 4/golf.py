a3d0bbeb7842458138a076094d218eba6ecd048a
Lucas Rodiadis <lucas2@live.com.au>
2022-12-04 21:31:40 +1100

    Condensed clamp

--- Day 4/golf.py
+++ Day 4/golf.py
@@ -1,9 +1,7 @@
 vals_list = [[int(val) for val in line.rstrip().replace(",", "-").split("-")] for line in open("input.txt", "r")]
 
 def clamp(low, high, contain_vals, *vals):
-	if contain_vals:
-		return len(vals) == 2 and low <= vals[0] <= vals[1] <= high
-	return any([low <= val <= high for val in vals])
+	return contain_vals and (len(vals) == 2 and low <= vals[0] <= vals[1] <= high) or (not contain_vals) and any([low <= val <= high for val in vals])
 
 total = sum([clamp(vals[0], vals[1], True, *vals[2:]) or clamp(vals[2], vals[3], True, *vals[:2]) for vals in vals_list])
 total_overlapping = sum([clamp(vals[0], vals[1], False, *vals[2:]) or clamp(vals[2], vals[3], False, *vals[:2]) for vals in vals_list])
