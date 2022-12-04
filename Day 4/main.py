909df1ceb83e07ed1250dfa4e6a37df0650b17d7
Lucas Rodiadis <lucas2@live.com.au>
2022-12-04 20:57:49 +1100

    Code golf for day 4

--- Day 4/main.py
+++ Day 4/main.py
@@ -1,29 +1,26 @@
 with open("input.txt", "r") as f:
 	lines = [line.rstrip() for line in f]
 
-total = 0
-
-total_overlapping = 0
-
 def clamp(low, high, contain_vals, *vals):
 	if contain_vals:
-		if len(vals) == 2:
-			return vals[0] >= low and vals[1] <= high
-		else:
-			return False
+		return len(vals) == 2 and low <= vals[0] <= vals[1] <= high
 	else:
-		for val in vals:
-			if low <= val <= high:
-				return True
-		return False
+		return any([low <= val <= high for val in vals])
+
+total = 0
+total_overlapping = 0
 
 for line in lines:
 	vals = [int(value) for value in line.replace(",", "-").split("-")]
-	if clamp(vals[0], vals[1], True, *vals[2:]) or clamp(vals[2], vals[3], True, *vals[:2]):
-		total += 1
-
-	if clamp(vals[0], vals[1], False, *vals[2:]) or clamp(vals[2], vals[3], False, *vals[:2]):
-		total_overlapping += 1
+	total += clamp(vals[0], vals[1], True, *vals[2:]) or clamp(vals[2], vals[3], True, *vals[:2])
+	total_overlapping += clamp(vals[0], vals[1], False, *vals[2:]) or clamp(vals[2], vals[3], False, *vals[:2])
 
 print(total)
 print(total_overlapping)
+
+	# if clamp(vals[0], vals[1], True, *vals[2:]) or clamp(vals[2], vals[3], True, *vals[:2]):
+	# 	total += 1
+
+	# if clamp(vals[0], vals[1], False, *vals[2:]) or clamp(vals[2], vals[3], False, *vals[:2]):
+	# 	total_overlapping += 1
+
