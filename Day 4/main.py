25e78232d0b6338d5d34483bb5ba747b2f33e565
Lucas Rodiadis <lucas2@live.com.au>
2022-12-04 20:48:00 +1100

    Using clamp function instead

--- Day 4/main.py
+++ Day 4/main.py
@@ -5,12 +5,24 @@ total = 0
 
 total_overlapping = 0
 
+def clamp(low, high, contain_vals, *vals):
+	if contain_vals:
+		if len(vals) == 2:
+			return vals[0] >= low and vals[1] <= high
+		else:
+			return False
+	else:
+		for val in vals:
+			if low <= val <= high:
+				return True
+		return False
+
 for line in lines:
 	vals = [int(value) for value in line.replace(",", "-").split("-")]
-	if vals[0] <= vals[2] <= vals[1] and vals[0] <= vals[3] <= vals[1] or vals[2] <= vals[0] <= vals[3] and vals[2] <= vals[1] <= vals[3]:
+	if clamp(vals[0], vals[1], True, *vals[2:]) or clamp(vals[2], vals[3], True, *vals[:2]):
 		total += 1
 
-	if vals[0] <= vals[2] <= vals[1] or vals[0] <= vals[3] <= vals[1] or vals[2] <= vals[0] <= vals[3] or vals[2] <= vals[1] <= vals[3]:
+	if clamp(vals[0], vals[1], False, *vals[2:]) or clamp(vals[2], vals[3], False, *vals[:2]):
 		total_overlapping += 1
 
 print(total)
