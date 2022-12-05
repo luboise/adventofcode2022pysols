a7bb430c2a42f3a18c0938ccdf834a526f2d5505
Lucas Rodiadis <lucas2@live.com.au>
2022-12-05 17:30:48 +1100

    Cleaned answer using functions

--- Day 5/main.py
+++ Day 5/main.py
@@ -1,61 +1,53 @@
-with open("input.txt", "r") as f:
-	lines = [line.rstrip() for line in f]
+import copy
 
-stacks = []
-moving = False
+def parseInput(filename):
+	with open(filename, "r") as f:
+		stacks = []
+		instructions = []
 
-for line in lines:
-	if not line:
-		continue
-	elif not stacks:
-		stacks = [list() for i in range((len(line) + 1) // 4)]
-	elif line.startswith(" 1"):
-		moving = True
-		continue
+		moving = False
 
+		for line in f:
+			# Ensure correct mode for parsing
+			line = line.rstrip()
+			if not stacks:
+				stacks = [list() for i in range((len(line) + 1) // 4)]
+			elif line.startswith(" 1") or not line:
+				moving = True
+				continue
 
-	if moving:
-		amount, move_from, move_to = tuple([int(value) for value in line[5:].replace("from ", "").replace("to ", "").split(" ")])
+			if moving:
+				# Add instruction tuple to stack
+				instruction = [int(value) for value in line[5:].replace("from ", "").replace("to ", "").split(" ")]
 
-		for i in range(amount):
-			stacks[move_to - 1].append(stacks[move_from - 1].pop())
-	else:
-		for i in range(0, (len(line) + 1) // 4):
-			char = line[4 * i + 1]
-			if char != " ":
-				stacks[i] = list(char) + stacks[i]
-	
-stacks2 = []
-moving = False
+				# Fix to/from to be indices
+				instruction[1] -= 1
+				instruction[2] -= 1
 
-for line in lines:
-	if not line:
-		continue
-	elif not stacks2:
-		stacks2 = [list() for i in range((len(line) + 1) // 4)]
-	elif line.startswith(" 1"):
-		moving = True
-		continue
+				instructions.append(instruction)
+			else:
+				# Append current char to appropriate stack
+				for i in range(0, (len(line) + 1) // 4):
+					char = line[4 * i + 1]
+					if char != " ":
+						stacks[i] = list(char) + stacks[i]
+				
 
+	return stacks, instructions
 
-	if moving:
-		amount, move_from, move_to = tuple([int(value) for value in line[5:].replace("from ", "").replace("to ", "").split(" ")])
+stacks, instructions = parseInput("input.txt")
+stacks2 = copy.deepcopy(stacks)
 
-		move_from -= 1
-		move_to -= 1
+for instruction in instructions:
+	# Adjust stack 1 (problem 1)
+	amount, move_from, move_to = tuple(instruction[:3])
+	for i in range(amount):
+		stacks[move_to].append(stacks[move_from].pop())
 
-		start_index = len(stacks2[move_from]) - amount
+	# Adjust stack 2 (problem 2)
+	start_index = len(stacks2[move_from]) - amount
+	stacks2[move_to] = stacks2[move_to] + stacks2[move_from][start_index:]
+	stacks2[move_from] = stacks2[move_from][:start_index]
 
-		stacks2[move_to] = stacks2[move_to] + stacks2[move_from][start_index:]
-		stacks2[move_from] = stacks2[move_from][:start_index]
-	else:
-		for i in range(0, (len(line) + 1) // 4):
-			char = line[4 * i + 1]
-			if char != " ":
-				stacks2[i] = list(char) + stacks2[i]
-
-to_print = [stack[-1] for stack in stacks if len(stack)]
-print(*to_print, sep = "")
-
-to_print2 = [stack[-1] if len(stack) else " " for stack in stacks2]
-print(*to_print2, sep = "")
+print(*[stack[-1] for stack in stacks if len(stack)], sep = "")
+print(*[stack[-1] for stack in stacks2 if len(stack)], sep = "")
