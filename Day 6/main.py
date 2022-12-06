343e49e90eb5f1ea5bf767e66b19e65192cce237
Lucas Rodiadis <lucas2@live.com.au>
2022-12-06 20:38:56 +1100

    functions

--- Day 6/main.py
+++ Day 6/main.py
@@ -1,11 +1,12 @@
-datastream = open("input.txt", "r").read().rstrip()
+def marker_finder(marker_len, filename):
+	datastream = open(filename, "r").read().rstrip()
+	for i in range(len(datastream) - marker_len):
+		chunk = datastream[i:i+marker_len]
+		if len(set(chunk)) == marker_len:
+			print(f"Marker from char {i+1} to char {i + marker_len}")
+			break
+	else:
+		print("Marker not found.")
 
-marker_len = 4
-
-for i in range(len(datastream) - marker_len):
-	chunk = datastream[i:i+marker_len]
-	if len(set(chunk)) == marker_len:
-		print(f"Marker from char {i+1} to char {i + marker_len}")
-		break
-else:
-	print("Marker not found.")
+marker_finder(4, "input.txt")
+marker_finder(14, "input.txt")
