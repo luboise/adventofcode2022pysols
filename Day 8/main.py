d29bfa1b2e94073deca028a5accc3cdcb2be26f4
Lucas Rodiadis <lucas2@live.com.au>
2022-12-09 16:59:26 +1100

    Day 8 solutions

--- Day 8/main.py
+++ Day 8/main.py
@@ -21,10 +21,50 @@ def is_visible(trees, row, col):
 	# default False
 	return False
 
-total_visible = 0
+def get_scenic_score(trees, row, col):
+	given_height = trees[row][col]
 
+	# left/right
+	left_val = 0
+	for i in range(col - 1, 0 - 1,-1):
+		left_val += 1 
+		if trees[row][i] >= given_height:
+			break
+		
+
+	right_val = 0
+	for i in range(col + 1,len(trees[row])):
+		right_val += 1 
+		if trees[row][i] >= given_height:
+			break
+		
+
+	# up/down
+	up_val = 0
+	for j in range(row - 1, 0 - 1,-1):
+		up_val += 1 
+		if trees[j][col] >= given_height:
+			break
+		
+	down_val = 0
+	for j in range(row + 1,len(trees)):
+		down_val += 1 
+		if trees[j][col] >= given_height:
+			break
+		
+	return left_val * right_val * down_val * up_val
+
+total_visible = 0
 for i in range(len(trees)):
 	for j in range(len(trees[0])):
 		total_visible += is_visible(trees, i, j)
 
 print(total_visible)
+
+max_score = 0
+for i in range(len(trees)):
+	for j in range(len(trees[0])):
+		score = get_scenic_score(trees, i, j)
+		if score > max_score:
+			max_score = score
+print(max_score)
