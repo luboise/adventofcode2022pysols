eb4a42512015d293ff048e7714185c359ae96271
Lucas Rodiadis <lucas2@live.com.au>
2022-12-14 15:46:06 +1100

    Day 11 part 1 solution

--- Day 11/main.py
+++ Day 11/main.py
@@ -1,30 +1,70 @@
-lines = [line.rstrip() for line in open("input.txt", "r")]
-
 class Monkey:
-	def __init__(self, items, execstring, divnum, truemonk, falsemonk):
+	def __init__(self, items, execstring, modnum, truemonk, falsemonk):
 		self.items = items
 		self.execstring = execstring
-		self.divnum = divnum
+		self.modnum = modnum
 		self.truemonk = truemonk
 		self.falsemonk = falsemonk
 
-monkeys = []
+		self.inspectioncount = 0
 
-i = 0
-while i < len(lines):
-	line = lines[i]
-	if line.startswith("Monkey"):
-		items = [int(worry) for worry in lines[i+1][len("  Starting items: "):].split(", ")]
-		execstring = lines[i+2][len("  Operation: new = "):]
-		divnum = int(lines[i+3][len("  Test: divisible by "):])
-		truemonk = int(lines[i+4][len("    If true: throw to monkey "):])
-		falsemonk = int(lines[i+5][len("    If false: throw to monkey "):])
+	def pass_items(self, monkey_list):
+		for i in range(len(self.items)):
+			self.increment_inspection()
+			exec("self.items[0] = " + self.execstring.replace("old", str(self.items[0])))
+			
+			self.items[0] = self.items[0] // 3
 
-		new_monkey = Monkey(items, execstring, divnum, truemonk, falsemonk)
-		monkeys.append(new_monkey)
+			recipient_index = self.get_passing_monkey(self.items[0])
+			monkey_list[recipient_index].give_item(self.items.pop(0))
 
-		i += 7
-	else:
-		i += 1
+	def get_passing_monkey(self, value):
+		if value % self.modnum == 0:
+			return self.truemonk
+		else:
+			return self.falsemonk
 
-print(monkeys)
+	def give_item(self, value):
+		self.items.append(value)
+
+	def increment_inspection(self, amount = 1):
+		self.inspectioncount += amount
+
+	def get_inspection_count(self):
+		return self.inspectioncount
+
+def get_monkeys_from_file(filename):
+	monkeys = []
+	
+	lines = [line.rstrip() for line in open(filename, "r")]
+
+	i = 0
+	while i < len(lines):
+		line = lines[i]
+		if line.startswith("Monkey"):
+			items = [int(worry) for worry in lines[i+1][len("  Starting items: "):].split(", ")]
+			execstring = lines[i+2][len("  Operation: new = "):]
+			modnum = int(lines[i+3][len("  Test: divisible by "):])
+			truemonk = int(lines[i+4][len("    If true: throw to monkey "):])
+			falsemonk = int(lines[i+5][len("    If false: throw to monkey "):])
+
+			new_monkey = Monkey(items, execstring, modnum, truemonk, falsemonk)
+			monkeys.append(new_monkey)
+
+			i += 7
+		else:
+			i += 1
+
+	return monkeys
+
+
+monkeys = get_monkeys_from_file("input.txt")
+
+
+for i in range(20):
+	for monkey in monkeys:
+		monkey.pass_items(monkeys)
+
+inspection_counts = sorted([monkey.get_inspection_count() for monkey in monkeys])
+
+print(inspection_counts[-2] * inspection_counts[-1])
